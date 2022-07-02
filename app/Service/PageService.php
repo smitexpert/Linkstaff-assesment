11d6fe549c9746db2e4a49db9534d66a20b541d1
Md Shanewyz Molla <smitexpert@gmail.com>
2022-07-02 18:23:57 +0600

    Add user_id on Page model

--- app/Service/PageService.php
+++ app/Service/PageService.php
@@ -3,12 +3,14 @@
 namespace App\Service;
 
 use App\Models\Page;
+use Illuminate\Support\Facades\Auth;
 
 class PageService {
     public function createPage($request)
     {
         $page = Page::create([
-            'page_name' => $request->page_name
+            'page_name' => $request->page_name,
+            'user_id' => Auth::user()->id,
         ]);
 
         return $page;
